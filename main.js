1cc96cd8af5194c040760ca9bab4589a8e63aede
Michael Lesniak <mlesniak@micromata.de>
2018-08-11 18:57:17 +0200

    Display text on sinus wave

--- main.js
+++ main.js
@@ -2,15 +2,36 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-// Drawn on each frame.
-function draw() {
+function initFrame() {
   background(0);
   stroke(255);
+  textSize(8);
+}
+
+function drawMessage(message) {
   for (let x = 0; x < windowWidth; x++) {
     let xPos = x;
     let yPos = windowHeight / 2 + Math.sin((x) / 60) * 40;
     point(xPos, yPos);
+
+    if (x % 10 != 0) {
+      continue;
+    }
+
+    let index = x / 10;
+    if (index > message.length) {
+      continue;
+    }
+    stroke(0);
+    fill(255);
+    text(message[index], xPos, yPos);
   }
+}
+
+// Drawn on each frame.
+function draw() {
+  initFrame();
+  drawMessage("Hello, world!");
 }
 
 function windowResized() {
