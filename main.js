079cc6124296c28fef4ed1eff1811d5e29e84e4a
Michael Lesniak <mlesniak@micromata.de>
2018-08-11 18:46:18 +0200

    Refactor variable names

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ function draw() {
   background(0);
   stroke(255);
   for (let x = 0; x < windowWidth; x++) {
-    point(x, windowHeight / 2 + Math.sin((x) / 60) * 40);
+    let xPos = x;
+    let yPos = windowHeight / 2 + Math.sin((x) / 60) * 40;
+    point(xPos, yPos);
   }
 }
 
