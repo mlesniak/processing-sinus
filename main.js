06355c28b71c5ada4d240ffe9eb4f6ae9f6b3d85
Michael Lesniak <mlesniak@micromata.de>
2018-08-11 18:45:02 +0200

    Move sinus wave

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
 }
 
 // Drawn on each frame.
 function draw() {
+  background(0);
   stroke(255);
   for (let x = 0; x < windowWidth; x++) {
-    point(x, windowHeight / 2 + Math.sin(x / 60) * 40);
+    point(x, windowHeight / 2 + Math.sin((x - frameCount) / 60) * 40);
   }
 }
 
